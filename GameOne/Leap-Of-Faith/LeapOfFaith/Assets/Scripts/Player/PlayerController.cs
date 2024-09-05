e515f0e5eb9e3fca553ef38b758b75bb5cace41f
Christian Rojas <koenigrojass@gmail.com>
2024-09-05 12:33:07 -0600

    Extra file changes

---
+++ GameOne/Leap-Of-Faith/LeapOfFaith/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Refrence to ground and air movement
+    private GroundMovement _groundMovement;
+    private AirMovement _airMovement;
+    
+    //If player is not grounded and falling use air movement
+    //If player is grounded use ground movement
+}
