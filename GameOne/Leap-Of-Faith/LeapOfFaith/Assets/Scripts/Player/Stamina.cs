ed7dc7c7cfcbb7ad332799e37453cda26ea83c6b
rougecode <koenigrojass@gmail.com>
2024-09-25 18:00:00 -0600

    Small change to stamina
    changed the name of a variabel

--- GameOne/Leap-Of-Faith/LeapOfFaith/Assets/Scripts/Player/Stamina.cs
+++ GameOne/Leap-Of-Faith/LeapOfFaith/Assets/Scripts/Player/Stamina.cs
@@ -5,7 +5,7 @@ public class Stamina : MonoBehaviour
 {
     [SerializeField] private float maxStamina = 100.0f; // Maximum stamina value
     [SerializeField] private float foodGain = 20.0f; // Stamina gained from food
-    private float currentStamina = 100.0f;
+    private float _currentStamina = 100.0f;
     private Health _health;
     
     private void Awake()
@@ -15,13 +15,13 @@ public class Stamina : MonoBehaviour
     
     private void Start()
     {
-        currentStamina = maxStamina; // Set current stamina to maximum stamina
+        _currentStamina = maxStamina; // Set current stamina to maximum stamina
     }
     
     private void Update()
     {
         //respawn player if stamina is 0
-        if (currentStamina <= 0)
+        if (_currentStamina <= 0)
         {
             _health.ResetPlayer();
         }
@@ -30,9 +30,9 @@ public class Stamina : MonoBehaviour
     public bool UsingStamina(float amount)
     {
         // Check if the player has enough stamina to use
-        if (currentStamina >= amount)
+        if (_currentStamina >= amount)
         {
-            currentStamina -= amount; // Decrease current stamina by the amount used
+            _currentStamina -= amount; // Decrease current stamina by the amount used
             return true;
         }
         return false;
@@ -40,12 +40,12 @@ public class Stamina : MonoBehaviour
     
     public float GetCurrentStamina()
     {
-        return currentStamina; // Return the current stamina value
+        return _currentStamina; // Return the current stamina value
     }
     
     private void GainStamina(float amount)
     {
-        currentStamina += amount; // Increase current stamina by the amount gained
+        _currentStamina += amount; // Increase current stamina by the amount gained
     }
 
     private void OnTriggerEnter(Collider other)
